3c81f8eb2cb97adcebe1f5ebe2a6cde821a1bce9
Rathan03 <74077198+Rathan03@users.noreply.github.com>
2026-09-23 18:42:12 +0100

    Added Market Data Tests

--- tests/marketdata_tests.cpp
+++ tests/marketdata_tests.cpp
@@ -8,10 +8,36 @@ using namespace option_pricing;
 TEST(MarketData_Tests, MarketData_Construction)
 {
     auto expected_timestamp = std::chrono::system_clock::now();
-    MarketData market_data{100,0.2,0.05, std::chrono::system_clock::now()};
+    MarketData market_data{100,0.2,0.05, expected_timestamp};
     EXPECT_EQ(market_data.get_rate(), 0.05);
     EXPECT_EQ(market_data.get_spot(), 100);
     EXPECT_EQ(market_data.get_vol(), 0.2);
     EXPECT_EQ(market_data.get_timestamp(), expected_timestamp);
 
+}
+
+TEST(MarketData_Tests, Invalid_Spot)
+{
+    auto construct_invalid = []()
+    {
+        MarketData market_data{0,0.2,0.05, std::chrono::system_clock::now()};
+    };
+
+    EXPECT_THROW(construct_invalid(), std::invalid_argument);
+}
+
+TEST(MarketData_Tests, Invalid_Vol)
+{
+    auto construct_invalid = []()
+    {
+        MarketData market_data{100,0,0.05, std::chrono::system_clock::now()};
+    };
+
+    EXPECT_THROW(construct_invalid(), std::invalid_argument);
+}
+
+TEST(MarketData_Tests, Negative_Rate)
+{
+    MarketData market_data{100,0.2,-0.05, std::chrono::system_clock::now()};
+    EXPECT_EQ(market_data.get_rate(), -0.05);
 }

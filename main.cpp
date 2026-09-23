41c161115c356ec03a6a6fa962bd84f7ef1317c4
Rathan03 <74077198+Rathan03@users.noreply.github.com>
2026-09-23 09:17:42 +0100

    Validate Black-Scholes pricing with put-call parity

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include "option_pricing/marketdata.h"
 #include "option_pricing/option.h"
 #include "option_pricing/blackscholes.h"
@@ -9,10 +10,12 @@ int main()
 {
     try
     {
-        Option option{OptionType::Put, 100, std::chrono::year_month_day{std::chrono::year{2027}, std::chrono::month{9}, std::chrono::day{23}}};
+        Option call{OptionType::Call, 100, std::chrono::year_month_day{std::chrono::year{2027}, std::chrono::month{9}, std::chrono::day{23}}};
+        Option put{OptionType::Put, 100, std::chrono::year_month_day{std::chrono::year{2027}, std::chrono::month{9}, std::chrono::day{23}}};
         MarketData market_data{100,0.2,0.05, std::chrono::system_clock::now()};
         BlackScholes model{market_data};
-        std::cout << model.option_price(option) <<'\n';
+        std::cout << model.option_price(call) - model.option_price(put) <<'\n';
+        std::cout << market_data.get_spot()-call.get_strike() * std::exp(- market_data.get_rate() * model.actual_365(call)) <<'\n';
 
     } catch (const std::exception& e)
     {
